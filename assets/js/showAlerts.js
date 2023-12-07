2a6623f38dfb33f2ac20f88bdba69a042bf02c85
fellipepatrick <fellipe.patrick@escolar.ifrn.edu.br>
2023-12-07 20:57:27 -0300

    versão final dos cards okay

--- assets/js/showAlerts.js
+++ assets/js/showAlerts.js
@@ -9,71 +9,72 @@ for(let alert in alerts){
     if(alerts[alert].email != null){
         const divCard = document.createElement('div');
         divCard.classList.add('card');
-
         const divLeft = document.createElement('div');
         divLeft.classList.add('left');
-
         const img = document.createElement('img');
         img.src = alerts[alert].path;
         const aNome =  document.createElement('a');
         aNome.classList.add('bold', 'name');
-        aNome.innerHTML = "autor: " + alerts[alert].name;
-
+        aNome.innerHTML =alerts[alert].name;
         const divGroup = document.createElement('div');
         divGroup.classList.add('group-info');
-
         const divInfoData = document.createElement('div');
         divInfoData.classList.add('info');
-
         const aData =  document.createElement('a');
         aData.classList.add('bold');
         const TextData = document.createElement('a');
         TextData.innerHTML = " Data: " + '&nbsp';
         const iData = document.createElement('i');
-        iData.classList.add('fa-regular', 'fa-clock');
+        iData.classList.add('fa-regular', 'fa-calendar');
         aData.append(iData);
         aData.appendChild(TextData);
-        
         const aInfoData = document.createElement('a');
-
         aInfoData.appendChild(aData);
-        aInfoData.innerHTML = alerts[alert].timer;
+        aInfoData.innerHTML = alerts[alert].data;
         divInfoData.appendChild(aData);
         divInfoData.appendChild(aInfoData);
-        divLeft.appendChild(divInfoData);
-
+        divGroup.appendChild(divInfoData);
         const divInfoHora = document.createElement('div');
         divInfoHora.classList.add('info');
-
         const aHora =  document.createElement('a');
         aHora.classList.add('bold');
         const Text = document.createElement('a');
         Text.innerHTML = " Data: " + '&nbsp';
         const iHora = document.createElement('i');
-        iHora.classList.add('fa-regular', 'fa-calendar');
+        iHora.classList.add('fa-regular', 'fa-clock');
         aHora.append(iHora);
         aHora.appendChild(Text);
-        
         const aInfoH = document.createElement('a');
-        
         aInfoH.appendChild(aHora);
-
-        aInfoH.innerHTML = alerts[alert].data;
-
+        aInfoH.innerHTML = alerts[alert].timer;
         divInfoHora.appendChild(aHora);
         divInfoHora.appendChild(aInfoH);
-        
-
-        divLeft.appendChild(divInfoHora);
-
+        divGroup.appendChild(divInfoHora);
+        const divMuni = document.createElement('div');
+        divMuni.classList.add('info');
+        const aMuni = document.createElement('a');
+        aMuni.classList.add('bold');
+        const iMuni = document.createElement('i');
+        iMuni.classList.add('fa-solid', 'fa-map-pin');
+        aMuni.appendChild(iMuni);
+        const aNomeMuni = document.createElement('a');
+        aNomeMuni.innerHTML = " Municipio: ";
+        aMuni.appendChild(aNomeMuni);
+        const aLocalMuni = document.createElement('a');
+        aLocalMuni.innerHTML = "&nbspMossoró";   
+        divMuni.appendChild(aMuni);
+        divMuni.appendChild(aLocalMuni);
+        divGroup.appendChild(divMuni);
         const divRight = document.createElement('div');
         divRight.classList.add('right');
-
-        divCard.appendChild(img);
-        divCard.appendChild(aNome);
+        const descricao = document.createElement('p');
+        descricao.innerHTML = alerts[alert].alerts;
+        divRight.appendChild(descricao);
+        divLeft.appendChild(img);
+        divLeft.appendChild(aNome);
+        divLeft.appendChild(divGroup);
         divCard.appendChild(divLeft);
         divCard.appendChild(divRight);
-
         section.appendChild(divCard);
     }
 }
